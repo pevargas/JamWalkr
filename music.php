58c2fc1bdc205dbad75202bcbea88e1c1fdf9ac3
root <patrick.vargas@colorado.edu>
2012-11-02 17:05:11 -0600

    JK, now I got it working... lolz...

--- music.php
+++ music.php
@@ -37,7 +37,27 @@
 	<pre>
 	  <?php
 	     #var_dump(curl_version());
-	     include ('mitch.txt'); 
+	     $curl = curl_init();
+	     // You can also set the URL you want to communicate with by doing this:
+	     // $curl = curl_init('http://localhost/echoservice');
+	     
+	     // We POST the data
+	     curl_setopt($curl, CURLOPT_POST, 1);
+	     // Set the url path we want to call
+	     curl_setopt($curl, CURLOPT_URL, 'http://localhost/JamWalkr/mitch.txt');  
+	     // Make it so the data coming back is put into a string
+	     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	     // Insert the data
+	     curl_setopt($curl, CURLOPT_POSTFIELDS, $some_data);
+	     
+	     // You can also bunch the above commands into an array if you choose using: curl_setopt_array
+	     
+	     // Send the request
+	     $result = curl_exec($curl);
+	     // Free up the resources $curl is using
+	     curl_close($curl);
+	     
+	     echo $result;
 	  ?>
 	</pre>
       </div>
--- using-curl.php
+++ using-curl.php
@@ -5,25 +5,4 @@
     'name' => 'Chad'
   );  
  
-  $curl = curl_init();
-  // You can also set the URL you want to communicate with by doing this:
-  // $curl = curl_init('http://localhost/echoservice');
-   
-  // We POST the data
-  curl_setopt($curl, CURLOPT_POST, 1);
-  // Set the url path we want to call
-  curl_setopt($curl, CURLOPT_URL, 'http://www.colorado.edu/cs/');  
-  // Make it so the data coming back is put into a string
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  // Insert the data
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $some_data);
-   
-  // You can also bunch the above commands into an array if you choose using: curl_setopt_array
-   
-  // Send the request
-  $result = curl_exec($curl);
-  // Free up the resources $curl is using
-  curl_close($curl);
-   
-  echo $result;
 ?>

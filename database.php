ab66ed814e15c100b5035d63d37f566595272b10
Casey Chevalier <Casey.Chevalier@Colorado.edu>
2012-11-07 10:24:34 -0700

    mySQL Database
    Ivona & I got the mySQL database
    working with the site. For now, we
    used a basic username and password for
    dynamically creating a database on a localhost
    but you'll need to go in and change that depending
    on what computer you're using for the demo. :-)

--- database.php
+++ database.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php include("res/auth.php");
-        include("res/loadfunc.php"); 
-	include("res/links.php") ?>
+    <meta charset="utf-8">
+    <title>JamWalkr</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- The styles America RULES-->
+    <link href="res/css/bootstrap.css" rel="stylesheet">
+    <link href="res/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
 </head>
 <body>
   <div class="navbar navbar-inverse navbar-fixed-top">
@@ -34,12 +46,106 @@
       </div>      <div class="span9">
 	<h1>MySQL Database</h1>
 	<pre>
-	  <?php
-	     echo "[Put Database Here!]";
-	  ?>
+		
+		
+		<!-- CHANGE USERNAME AND PASSWORD FOR NOW FOR LOCALHOST USE -->
+		<?php
+			$username="root";
+			$password="password";
+			$database="JamWalkr";
+		?> 
+		
+		
+
+		<?php
+
+			include("res/auth.php");
+			include("res/loadfunc.php");
+			mysql_connect(localhost,$username,$password);
+
+
+			$con = mysql_connect(localhost,$username,$password);
+
+			if (!$con)
+			  {
+			  die('Could not connect: ' . mysql_error());
+			  }
+
+			if (mysql_query("CREATE DATABASE $database",$con))
+			  {
+			  echo "Database created";
+			  }
+			else
+			  {
+			  echo "Error creating database: " . mysql_error();
+			  }
+  
+			@mysql_select_db($database) or die( "Unable to select database in setup.php");
+ 
+			 $query="CREATE TABLE Artists (id int(6) PRIMARY KEY NOT NULL auto_increment, name varchar(30) NOT NULL, img varchar(60) NOT NULL)";
+			 mysql_query($query);
+ 
+			# var_dump(curl_version());
+			   $lfmmethod = "chart.getTopArtists";
+			   $url = $lfmbase . "?method=" . $lfmmethod . $lfmkey;
+			   echo "<p class='lead'>" . $lfmmethod . "</p>";	   
+
+			   $response = get_page($url);
+			   $xml = new SimpleXMLElement($response);
+			   
+			   $data = $xml->artists->artist; 
+			   for ($i = 0; $i < sizeof($data); $i++) { 
+			
+				 $name = (string) $data[$i]->name;
+				 $img  = $data[$i]->children();
+				 $img  = (string) $img->image[3];
+				
+				 $query = "INSERT INTO Artists VALUES ('', '$name', '$img')";
+				 //$sql = 'INSERT INTO `test` ( `title` , `BODY` ) ' . "VALUES ( '$title', '$body' 
+				 mysql_query($query);
+				 }
+
+				mysql_close(); 
+				
+				// run initializer to set $username, $password, $database
+				mysql_connect(localhost,$username,$password);
+				@mysql_select_db($database) or die( "Unable to select database");
+				$query="SELECT * FROM Artists";
+				$result=mysql_query($query);
+				
+				// recover ALL the things from this broad query
+				$num=mysql_numrows($result); 	
+
+		echo "<b><center>Last.fm Database</center></b><br><br>";
+
+		?>
+
+		
+		<?php
+		$i=0;
+		while ($i < $num) {
+			$name=mysql_result($result,$i,"name");
+			$img=mysql_result($result,$i,"img");
+		?>
+		
+		
+		<?php
+		 echo "<div class='media'>";
+             echo "<img src='" . $img . "' alt='" . $name . "' class='media-object thumbnail'/></a>";
+	     echo "<div class='media-body'>";
+	     echo "<h2 class='media-heading'>" . $name . "</h2>";
+	     echo "</div></div>";
+		?>
+		
+
+		<?php
+		++$i;
+		} 
+		?>
 	</pre>
       </div>
     </div>
   </div>
+  <?php include ('res/js/scripts.js'); ?>
 </body>
 </html>

851a2856669d27dd45135da8e1278cd44eb487c7
Ivona Andrzejewski <ivona.andrzejewski@gmail.com>
2012-11-27 21:02:44 -0700

    Added query function

---
+++ res/php/query.php
@@ -0,0 +1,15 @@
+<?php
+	include("res/php/auth.php");
+
+	if (isset($_REQUEST["sql"]) || !($_REQUEST["sql"] == "")) { 
+		
+		mysql_connect($mysql_host,$username,$password);
+		$con = mysql_connect($mysql_host,$username,$password);	     
+		if (!$con) {
+			die("<div class='alert alert-error'><button type='button' class='close' data-dismiss='alert'>×</button><strong>Error: </strong>" . mysql_error() . "</strong></div>");
+		}
+
+		mysql_query($_REQUEST["sql"]);
+
+	}
+?>

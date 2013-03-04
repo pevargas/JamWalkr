fe917669e193c31deae1a384ee458247eb9303b1
Patrick <pevargas90@gmail.com>
2013-03-04 14:57:24 -0700

    Pulled what was on the server.

--- map.php
+++ map.php
@@ -1,225 +1,173 @@
+<!--
+  map.php
+
+  by Mitchell Goudy
+  (mitchell.a.goudy@gmail.com)
+
+  with help from: Casey Chevalier, Ivona Andrzejewski, and Patrick Vargas 
+
+  This file should be helpful for a newcomer to learn some of the features
+  the Google Maps API offers, and how they can use it effectively in their
+  own applications.
+-->
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <?php
-    include("res/php/auth.php");
-    include("res/php/loadfunc.php"); 
-    include("res/php/links.php");
-  ?>   
+  <head>
 
-   <style type="text/css">
-    #map_canvas {
-      margin: 0;
-      margin-top: 40px;
-      padding: 0;
-      width: 100%;
-    }
-  </style>
+    <!--
+    Some basic styling to define what the map will look like
+    -->
+    <style type="text/css">
+      body, html {
+        height: 100%;
+        width: 100%;
+      }
 
-<?php 
-  mysql_connect($mysql_host,$username,$password);
-  $con = mysql_connect($mysql_host,$username,$password);       
-  if (!$con) {
-    die("<div class='alert alert-error'><button type='button' class='close' data-dismiss='alert'>×</button><strong>Error: </strong>" . mysql_error() . "</strong></div>");
-  }
-  $db = mysql_select_db($database);
-  ?>
+      div#map_canvas{
+        width: 100%; height: 100%;
+        margin: 0;
+        margin-top: 0px;
+        padding: 0;
+      }
 
-<?php 
-  $sql = 'SELECT * FROM `Buildings` LIMIT 0, 30 ';
-  $rs = mysql_query($sql);
-    
-  if (!$rs) { die("<div class='alert alert-error'><button type='button' class='close' data-dismiss='alert'>×</button><strong>Error: </strong>" . mysql_error() . "</strong></div>"); } ?>
+    </style>
 
-  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-  <script type="text/javascript">
-    var map;
-    function initialize() {
-      // Init map options
-      var mapOptions = {
-        zoom: 14,
-        center: new google.maps.LatLng(40.0150, -105.2700),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    <!--
+    The API itself is hostly reliably by Google. This line is all you need to 
+    start using it.
+    -->
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 
-      // Markers
-      var id;
-      var markers = {};
+    <!--
+    Here's where we will use the API, and write some functionality for it
+    -->
+    <script type="text/javascript">
+      var map;
 
-      var infowindow = new google.maps.InfoWindow();
+      /*  This will wait until the page is fully loaded, then call an 
+       *  'initialize' function, which we'll define later. This is
+       *  crucial to preventing race conditions as various scripts
+       *  are loaded on the page.
+       */
+      google.maps.event.addDomListener(window, 'load', initialize);
 
-      // Add Init markers
-      var addInitMarker = function (alatlng, name, id, tagarr, ratingarr) {
-          marker = new google.maps.Marker({ 
+      function initialize() {
+
+        // Init map options
+        var mapOptions = {
+          // How zoomed in the map will be
+          zoom: 14, 
+          // The intial center of the map, I chose Boulder here
+          center: new google.maps.LatLng(40.0150, -105.2700),
+          // What sort of map it will be
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        // Define which element on the page we want our map to associate with
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+        //That is actually all we need for a basic map. To make things interesting,
+        //let's add some markers to our map.
+
+        // Marker variables
+        var id; //id/handle give to each marker so that we may access it later
+        var markers = {}; // array of all markers
+        var infowindow = new google.maps.InfoWindow(); // A window we can open for each marker
+
+        /*
+         *  We need to create a 'listener', a script that will run on the page,
+         *  and wait until a action occurs. in this case, I'll listen for a 
+         *  right click, and then run some code when that happens
+         */
+        google.maps.event.addListener(map, "rightclick", function(event) {
+
+          //We can capture the lat/long data of where the right click happened
+          var lat = event.latLng.lat();
+          var lng = event.latLng.lng();
+
+          //Create a LatLng object. This is really just a simple data structure
+          //with a lat and long field that we want ot associate together
+          var myLatlng = new google.maps.LatLng(lat, lng);
+
+          //Call an addMarker function to add a marker where we clicked
+          addMarker(myLatlng);
+        });
+
+        // Add marker function
+        var addMarker = function (alatlng) {
+          //Define a new marker object          
+          marker = new google.maps.Marker({
+              //The position we captured earlier
               position: alatlng,
+              //The map we've been using
               map: map,
+              //Make the marker draggable after it's been placed
               draggable: true,
+              //Add a fun animation when it is placed
               animation: google.maps.Animation.DROP
           });
 
-          var contentString = "<h3>" + name + "</h3>";
-          var first = true;
-          var playTags;
-          for(var tag in tagarr){
-            if (first) { playTags = tagarr[tag]; first = false; }
-            else { playTags += "+" + tagarr[tag]; }
-            contentString += "<span class='badge badge-jam'>" + tagarr[tag] + " ( " + ratingarr[tag] + " )</span> ";
-          }
-          contentString += "<p>"+playTags+"</p>";
-
-          contentString += "<div>" +
-              "<button class='btn btn-jam control-conatiner pull-left'>" +
-                "<i onclick='toggleMusic()' id='control' class='icon-pause icon-white'></i>" +
-              "</button>" + 
-              "<div class='progress progress-jam progress-striped active'>" +
-                "<div class='bar' id='time'><span id='current' class='badge badge-jam'></span></div>" +
-              "</div></div>";
-
+          // Here's some content we're going to associate with the marker, and display when the
+          // marker is clicked on
+          var contentString = "<h3>Hello!</h3>" + 
+                              "<p>This frame can contain any of the HTML content you would put anywhere else on the page</p>"+
+                              "<button>Like a button!</button>"+
+                              "<input value=' or a text input field'></input>";
+          
+          // Associate our content with our marker and make a handler for it
           makeInfoWindowEvent(map, infowindow, contentString, marker);
 
+          // Create a unique id for the marker and add it to the marker array
           id = marker.__gm_id
           markers[id] = marker; 
 
-          google.maps.event.addListener(marker, "rightclick", function (point) { 
-            id = this.__gm_id; 
+          // Now let's add a listener for when an existing marker is right clicked
+          google.maps.event.addListener(marker, "rightclick", function (point) {
+            //Recall the id we want with the 'this' handle
+            id = this.__gm_id;
+            //Call a delete marker function
             delMarker(id);
           });
+
+          // And one more listener for the left click on an existing marker
           google.maps.event.addListener(marker, 'click', function(point) {
+            //Recall the id we want with the 'this' handle
             id = this.__gm_id;
+            //open an 'infowindow', which will show that content we defined earlier
             infowindow.open(map, markers[id]);
           });
+        }
 
-          var circleSize = 1;
-          for(var tag in tagarr){
-             circleSize += Number(ratingarr[tag]);
-          }
-          console.log(circleSize);
-          var circleOptions = {
-              strokeColor: "#8800CC",
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: "#8800CC",
-              fillOpacity: 0.35,
-              map: map,
-              center: alatlng,
-              radius: circleSize * 50
-            };
-            musicCircle = new google.maps.Circle(circleOptions);
-
-      }
-
-      // Add markers
-      var addMarker = function (alatlng) {
-          marker = new google.maps.Marker({ 
-              position: alatlng,
-              map: map,
-              draggable: true,
-              animation: google.maps.Animation.DROP
-          });
-
-          var contentString = "<form class='addPlace' method='get' action='res/php/add.php'>" + 
-            "<input type='text' name='name' placeholder='Name of Building' autofocus='autofocus'/><br/>" +
-            "<input type='text' name='tag1' id='tag' placeholder='mood, genre, or artist'/><br/>" +
-            "<input type='text' name='tag2' id='tag' placeholder='mood, genre, or artist'/><br/>" +
-            "<input type='text' name='tag3' id='tag' placeholder='mood, genre, or artist'/><br/>" +
-            "<input type='text' name='lat' value='"+alatlng.lat()+"' style='display:none;'/><br/>" +
-            "<input type='text' name='lng' value='"+alatlng.lng()+"' style='display:none;'/>" +
-            "<button type='submit' class='btn btn-jam'>Save</button></form>";
-          
-          makeInfoWindowEvent(map, infowindow, contentString, marker);
-
-          //map.panTo(alatLng);
-          id = marker.__gm_id
-          markers[id] = marker; 
-
-          google.maps.event.addListener(marker, "rightclick", function (point) { id = this.__gm_id; delMarker(id) });
-          google.maps.event.addListener(marker, 'click', function(point) {
-            id = this.__gm_id;
-            infowindow.open(map, markers[id]);
-          });
-      }
-
-      // Delete marker on right click
-      var delMarker = function (id) {
-          marker = markers[id]; 
+        // Here's the delete function that gets called when you right click a marker
+        var delMarker = function (id) {
+          //lookup which marker it is we want to kill, from our markers array
+          marker = markers[id];
+          //And eliminate it!
           marker.setMap(null);
+        }
+
+        // Here's where we get our marker ready to display some content when it's clicked
+        function makeInfoWindowEvent(map, infowindow, contentString, marker) {
+          //Once again, define a page listener for when the marker is clicked
+          google.maps.event.addListener(marker, 'click', function() {
+            // set what the actual content is we want to display
+            infowindow.setContent(contentString);
+            //and show it!
+            infowindow.open(map, marker);
+          });
+        }
       }
+      /*
+      *  That's it!
+      *  There are millions of things you can do from here, take a look at the API
+      *  docs for more ideas, and good luck! (https://developers.google.com/maps/)
+      */
+    </script>
+  </head>
+  <body>
+    <!-- Here's the target for all of our map scripting-->
+    <div id="map_canvas"></div>
 
-      function makeInfoWindowEvent(map, infowindow, contentString, marker) {
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(contentString);
-          infowindow.open(map, marker);
-        });
-      }
-
-      // Right click handler
-      google.maps.event.addListener(map, "rightclick", function(event) {
-        var lat = event.latLng.lat();
-        var lng = event.latLng.lng();
-        var myLatlng = new google.maps.LatLng(lat, lng);
-        addMarker(myLatlng);
-      });
-
-
-<?php while($row = mysql_fetch_array($rs)) { ?>
-        <?php $sql2 = "SELECT * FROM `Tags` WHERE `building` = '".$row['id']."'";
-          $rs2 = mysql_query($sql2);
-          if (!$rs2) { die("<div class='alert alert-error'><button type='button' class='close' data-dismiss='alert'>×</button><strong>Error: </strong>" . mysql_error() . "</strong></div>"); } 
-          ?>
-          var tagarr = [];
-          var ratingarr = [];
-          var i = 0;
-        <?php while($row2 = mysql_fetch_array($rs2)) { ?>
-            tagarr[i] = "<?=$row2['tag']?>";
-            ratingarr[i] = "<?=$row2['rating']?>";
-            i++;
-        <?php } ?>
-        addInitMarker(new google.maps.LatLng(<?=$row['lat']?>, <?=$row['lng']?>), "<?=$row['name']?>", <?=$row['id']?>, tagarr, ratingarr);
-<?php } ?>
-    
-    $("#map_canvas").css("height", window.innerHeight - 40);
-  
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-  </script>
-
-</head>
-<body>
-  <div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
-      <div class="container-fluid">
-        <!-- Button to trigger modal -->
-        <a href="#help" role="button" class="btn btn-jam" data-toggle="modal">Need Help?</a>
-        <div class="pull-right"><a class="brand" href="./index.php">JamWalkr</a></div>
-      </div>
-    </div>
-  </div>
-  
-  <span id="msg"></span>
-  <video id="player" class="data"></video>
-  <div id="map_canvas"></div>
- 
-  <!-- Modal -->
-  <div id="help" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-      <h3 id="myModalLabel">Welcome to JamWalkr</h3>
-    </div>
-    <div class="modal-body">
-      <p class="lead">What is JamWalkr?</p>
-      <p>JamWalkr is a music tagging application. Users are encouraged to tag buildings with moods, genres or artists. As the number of tagged buildings grow, we can see the citezens of a city thinks their city sounds.</p>
-
-      <p class="lead">How do I participate?</p>
-      <p>To add a point, simiply right click and fill out the form. You can view already tagged buildings by clicking on their markers. Please also vote on whether you agree with the tags buy voting up or down the specific tags.</p>
-    </div>
-    <div class="modal-footer">
-      <button class="btn btn-jam" data-dismiss="modal" aria-hidden="true">Start Jammin'</button>
-    </div>
-  </div>
-
-  <a href="http://www.000webhost.com/" target="_blank" class="host"><img src="http://www.000webhost.com/images/80x15_powered.gif" alt="Web Hosting" width="80" height="15" border="0" /></a>
-</body>
+  </body>
 </html>
